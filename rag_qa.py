6fcb56e132757cb3161805541d8f3002729a02f8
Afrah <afrah.abdullahih@gmail.com>
2024-01-29 13:58:33 -0500

    embeddings update

--- rag_qa.py
+++ rag_qa.py
@@ -6,6 +6,7 @@ from langchain import hub
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.prompts import PromptTemplate
 from langchain_community.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain_openai import OpenAIEmbeddings
 from langchain.tools.retriever import create_retriever_tool
 from langchain.agents import AgentExecutor, create_openai_tools_agent
